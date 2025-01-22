a4d6362daf7e9164fa91f5e1b3ef22ab5f083593
Muhammad Hamza <160543204+muhammadhamza78@users.noreply.github.com>
2025-01-22 12:29:52 +0500

    Update Fetch.ts

--- src/sanity/lib/Fetch.ts
+++ src/sanity/lib/Fetch.ts
@@ -1,12 +1,18 @@
 import { createClient } from "next-sanity";
 
-const client = createClient ({
-    projectId: "o4pi99er",
-    dataset: "production",
-    useCdn:true,
-    apiVersion: "2025-01-15"
-})
+const client = createClient({
+  projectId: "o4pi99er",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2025-01-15",
+});
 
-export async function sanityFetch({query, params ={}}:{query:string, params?:any}){
-return await client.fetch (query,params)
+export async function sanityFetch({
+  query,
+  params = {},
+}: {
+  query: string;
+  params?: Record<string, any>; // Specify a more general type for params
+}) {
+  return await client.fetch(query, params);
 }
