ff37030239e1721526fe9d13122c4983a969faff
Muhammad Hamza <160543204+muhammadhamza78@users.noreply.github.com>
2025-01-22 12:46:32 +0500

    Update Cont.tsx

--- src/components/Cont.tsx
+++ src/components/Cont.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import Image from 'next/image' // Importing the Image component from next/image
 
 const Cont = () => {
   return (
     <div>
-      <img className="w-[904] h-[93] mx-40 pt-20"
-      src="cont1.svg" alt="cont1" />
-      
+      <div className="w-[904px] h-[93px] mx-40 pt-20 relative">
+        <Image
+          src="/cont1.svg" // Path to your image
+          alt="cont1"
+          layout="fill" // Ensures the image fills the parent container
+          objectFit="contain" // Ensures the aspect ratio is maintained
+        />
+      </div>
     </div>
   )
 }
 
 export default Cont
-
-
-
