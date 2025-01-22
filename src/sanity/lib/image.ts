f1722456af856965b5e8d112c3e3adb7257bcf62
Muhammad Hamza <160543204+muhammadhamza78@users.noreply.github.com>
2025-01-22 14:23:24 +0500

    Update image.ts

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,11 +1,34 @@
+// import createImageUrlBuilder from '@sanity/image-url'
+// import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+// import { dataset, projectId } from '../env'
+
+// // https://www.sanity.io/docs/image-url
+// const builder = createImageUrlBuilder({ projectId, dataset })
+
+// export const urlFor = (source: SanityImageSource) => {
+//   return builder.image(source)
+// }
+
+
+
+
+
+
 import createImageUrlBuilder from '@sanity/image-url'
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
 import { dataset, projectId } from '../env'
 
-// https://www.sanity.io/docs/image-url
+// Initialize the image URL builder
 const builder = createImageUrlBuilder({ projectId, dataset })
 
+// The urlFor function to generate the image URL
 export const urlFor = (source: SanityImageSource) => {
-  return builder.image(source)
+  if (source) {
+    // Ensure source contains _type property, otherwise provide a fallback
+    const imageSource = source._type === 'image' ? source : { ...source, _type: 'image' }
+    return builder.image(imageSource)
+  }
+  return null // Return null if no valid source is provided
 }
